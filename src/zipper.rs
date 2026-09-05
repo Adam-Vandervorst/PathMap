8eb46ed49cada63d1bf8e7e24e861062ec9f16ab
Luke Peterson <luke@infoplatter.com>
2026-09-05 07:29:22 -0600

    Fixing a fat-finger bug introduced a couple commits ago.

--- src/zipper.rs
+++ src/zipper.rs
@@ -1731,7 +1731,6 @@ pub(crate) mod read_zipper_core {
                 debug_assert!(self.is_regularized());
                 true
             } else {
-                self.focus_iter_token = new_tok;
                 debug_assert!(self.is_regularized());
                 false
             }
