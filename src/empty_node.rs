fa6cb9b2f81713bea62e795acd2fbd0f651f228b
Luke Peterson <luke@infoplatter.com>
2026-09-06 07:30:34 -0600

    Fixing EmptyNode's implementation of iter_token_for_path

--- src/empty_node.rs
+++ src/empty_node.rs
@@ -61,8 +61,13 @@ impl<V: Clone + Send + Sync, A: Allocator> TrieNode<V, A> for EmptyNode {
     fn new_iter_token(&self) -> IterToken {
         0
     }
-    fn iter_token_for_path(&self, _key: &[u8]) -> IterToken {
-        0
+    #[inline]
+    fn iter_token_for_path(&self, key: &[u8]) -> IterToken {
+        if key.len() == 0 {
+            0
+        } else {
+            NODE_TOKEN_NONEXISTENT_BIT
+        }
     }
     fn ascend_iter_token(&self, _token: IterToken, _byte_count: usize) -> IterToken {
         unreachable!()
