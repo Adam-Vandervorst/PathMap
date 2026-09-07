14b25dd2c4d625e6ff1d61767bd4cad21ea0ab2d
Marcin Rzeźnicki <marcin.rzeznicki@gmail.com>
2026-09-07 10:26:09 +0200

    splitting the API into ZipperValue<V> and ZipperValueAt<V>
    Split zipper_moving_tests to follow the new ValuesAt trait

--- src/poly_zipper.rs
+++ src/poly_zipper.rs
@@ -109,6 +109,15 @@ mod tests {
         }
     );
 
+    crate::zipper::zipper_moving_tests::zipper_val_at_tests!(poly_zipper_pm,
+        |keys: &[&[u8]]| {
+            keys.iter().map(|k| (k, ())).collect::<PathMap<()>>()
+        },
+        |btm: &mut PathMap<()>, path: &[u8]| -> _ {
+            TestPolyZipper::PathMapU(btm.read_zipper_at_path(path))
+        }
+    );
+
     crate::zipper::zipper_iteration_tests::zipper_iteration_tests!(poly_zipper_pm,
         |keys: &[&[u8]]| {
             keys.iter().map(|k| (k, ())).collect::<PathMap<()>>()
@@ -120,6 +129,17 @@ mod tests {
 
     #[cfg(feature = "arena_compact")]
     crate::zipper::zipper_moving_tests::zipper_moving_tests!(poly_zipper_act,
+        |keys: &[&[u8]]| {
+            let btm = keys.iter().map(|k| (k, ())).collect::<PathMap<()>>();
+            ACTVec::from_zipper(btm.read_zipper(), |()| 0)
+        },
+        |act: &mut ACTVec, path: &[u8]| -> _ {
+            TestPolyZipper::ACTVecPrefix(PrefixZipper::new(&[], act.read_zipper_at_path(path)))
+        }
+    );
+
+     #[cfg(feature = "arena_compact")]
+    crate::zipper::zipper_moving_tests::zipper_val_at_tests!(poly_zipper_act,
         |keys: &[&[u8]]| {
             let btm = keys.iter().map(|k| (k, ())).collect::<PathMap<()>>();
             ACTVec::from_zipper(btm.read_zipper(), |()| 0)
@@ -143,7 +163,7 @@ mod tests {
     // ======================================================================================
     // Cocktail of recursive zipper madness
     #[derive(PolyZipperExplicit)]
-    #[poly_zipper_explicit(traits(Zipper, ZipperValues, ZipperMoving, ZipperPath, ZipperIteration))]
+    #[poly_zipper_explicit(traits(Zipper, ZipperValues, ZipperValuesAt, ZipperMoving, ZipperPath, ZipperIteration))]
     pub enum ExprFactor<'trie, V: Clone + Send + Sync + Unpin + 'static = ()> {
         Specific(ReadZipperOwned<V>),
         Generic(PrefixZipper<'trie,
@@ -157,6 +177,15 @@ mod tests {
     }
 
     crate::zipper::zipper_moving_tests::zipper_moving_tests!(recursive_zipper_madness,
+        |keys: &[&[u8]]| {
+            keys.iter().map(|k| (k, ())).collect::<PathMap<()>>()
+        },
+        |btm: &mut PathMap<()>, path: &[u8]| -> _ {
+            ExprFactor::Specific(btm.clone().into_read_zipper(path))
+        }
+    );
+
+    crate::zipper::zipper_moving_tests::zipper_val_at_tests!(recursive_zipper_madness,
         |keys: &[&[u8]]| {
             keys.iter().map(|k| (k, ())).collect::<PathMap<()>>()
         },
--- src/trie_ref.rs
+++ src/trie_ref.rs
@@ -266,6 +266,9 @@ impl<V: Clone + Send + Sync + Unpin, A: Allocator> ZipperValues<V> for TrieRefBo
     fn val(&self) -> Option<&V> {
         self.get_val()
     }
+}
+
+impl<V: Clone + Send + Sync + Unpin, A: Allocator> ZipperValuesAt<V> for TrieRefBorrowed<'_, V, A> {
     fn val_at<K: AsRef<[u8]>>(&self, path: K) -> Option<&V> {
         self.get_val_at(path)
     }
@@ -646,6 +649,9 @@ impl<V: Clone + Send + Sync + Unpin, A: Allocator> ZipperValues<V> for TrieRefOw
             None
         }
     }
+}
+
+impl<V: Clone + Send + Sync + Unpin, A: Allocator> ZipperValuesAt<V> for TrieRefOwned<V, A> {
     fn val_at<K: AsRef<[u8]>>(&self, path: K) -> Option<&V> {
         if self.is_valid() {
             TrieRefBorrowed::new_with_key_and_path_in(
@@ -844,6 +850,9 @@ impl<V: Clone + Send + Sync + Unpin, A: Allocator> ZipperValues<V> for TrieRef<'
             TrieRef::Owned(trie_ref) => trie_ref.val(),
         }
     }
+}
+
+impl<V: Clone + Send + Sync + Unpin, A: Allocator> ZipperValuesAt<V> for TrieRef<'_, V, A> {
     fn val_at<K: AsRef<[u8]>>(&self, path: K) -> Option<&V> {
         match self {
             TrieRef::Borrowed(trie_ref) => trie_ref.val_at(path),
@@ -1138,7 +1147,7 @@ mod tests {
 
     #[test]
     fn trie_ref_val_at_test() {
-        fn assert_val_at<T: ZipperValues<i32>>(trie_ref: T) {
+        fn assert_val_at<T: ZipperValuesAt<i32>>(trie_ref: T) {
             assert_eq!(trie_ref.val(), None);
             assert_eq!(trie_ref.val_at(b"root:a:new_a"), Some(&10));
             assert_eq!(trie_ref.val_at(b"root:a:nested:deep"), Some(&11));
