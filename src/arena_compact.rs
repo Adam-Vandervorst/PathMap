d7bd8ea27db4863ee5ab43a59fa301d06a5ad420
Luke Peterson <luke@infoplatter.com>
2026-09-11 17:51:09 -0600

    Restore ACT zipper iteration documentation

--- src/arena_compact.rs
+++ src/arena_compact.rs
@@ -3213,7 +3213,17 @@ where Storage: AsRef<[u8]>
         }
     }
 
-    /// Moves the zipper's focus to the next location with the same path length as the current focus.
+    /// Moves the zipper's focus to the next location with the same path length as the current focus,
+    /// following a depth-first exploration from a common root `k` steps above the current focus
+    ///
+    /// Returns `true` if the zipper has sucessfully moved to a new location at the same level, or `false`
+    /// if no further locations exist.  If this method returns `false` then the zipper will be ascended `k`
+    /// steps to the common root.  (The focus position when [descend_first_k_path](ZipperIteration::descend_first_k_path) was called)
+    ///
+    /// WARNING: This is not a constant-time operation, and may be as bad as `order n` with respect to the paths
+    /// below the zipper's focus.  Although a typical cost is `order log n` or better.
+    ///
+    /// See: [descend_first_k_path](ZipperIteration::descend_first_k_path)
     fn to_next_k_path(&mut self, k: usize) -> bool {
         timed_span!(ToNextKPath, COUNTERS);
         let mut depth = k;
